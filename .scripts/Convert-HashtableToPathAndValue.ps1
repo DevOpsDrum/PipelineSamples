ed7c3b7cafceaa31cfa4065a42007b3df3cb9a41
Mike <96633619+DevOpsDrum@users.noreply.github.com>
2022-03-07 16:28:12 -0500

    Update Convert-HashtableToPathAndValue.ps1
    cleaned up script

--- .scripts/Convert-HashtableToPathAndValue.ps1
+++ .scripts/Convert-HashtableToPathAndValue.ps1
@@ -1,39 +1,52 @@
 <#
 .DESCRIPTION
-    Convert hashtable into two column table with path and value
+    Convert hashtable into single-level Object[] with property name path and value
 .EXAMPLE
-    PS> .scripts/Convert-HashtableToPathAndValue.ps1 -Hashtable <hashtable object>
-    Iterate through hashtable and and display report with dot (.) notation path and [string] values 
+    PS> .pipelines/scripts/Convert-HashtableToPathAndValue.ps1 `
+        -Hashtable (Get-Content -Raw -Path "${HOME}/repos/myRepo/myConfig.yaml" | ConvertFrom-Yaml)
+    Iterate through hashtable and create single-level Object[] with property name path and value, e.g. $Hashtable.propertyOne = 12345
 .OUTPUTS
     [Object[]]
-    | path | value |
-    | ---- | ----- |
-    | $Hashtable.propertyOne | 12345 |
-    | $Hashtable.PropertyTwo.Level2 | ABCDEFT |
-    
+    * e.g.
+        | Name                          | Value   |
+        | ----------------------------- | ------- |
+        | $Hashtable.propertyOne        | 12345   |
+        | $Hashtable.PropertyTwo.Level2 | ABCDEFG |
 .NOTES
-    Script is intended to create an object that can be used to compare two JSON or YAML objects.
+    * Script is intended to create an object that can be used to compare JSON or YAML objects, like two config files
+    * Convert JSON file:
+        ```
+        $jsonObject = Get-Content -Raw -Path "${HOME}/repos/myRepo/myConfig.json" | ConvertFrom-Json
+        ./.pipelines/scripts/Convert-HashtableToPathAndValue.ps1 -Hashtable $jsonObject | Format-Table -AutoSize -Wrap
+        ```
+    * Convert YAML file:
+        ```
+        Import-Module powershell-yaml
+        $yamlObject = Get-Content -Raw -Path "${HOME}/repos/myRepo/myConfig.yaml" | ConvertFrom-Yaml
+        ./.pipelines/scripts/Convert-HashtableToPathAndValue.ps1 -Hashtable $yamlObject | Format-Table -AutoSize -Wrap
+        ```
 #>
 
 [CmdletBinding()]
-[OutputType([System.Object[]])]
+[OutputType([Object[]])]
 param(
-    [Parameter(HelpMessage = 'Hashtable object to be converted')]
-    [hashtable] $Hashtable
+    [Parameter(HelpMessage = 'Hashtable object to be converted', ValueFromPipeline)]
+    [ValidateNotNullOrEmpty()]
+    [Hashtable] $Hashtable = (Get-Content -Raw -Path "${HOME}/repos/RP-Config/deploy/int-config.yaml" | ConvertFrom-Yaml)
 )
 
 begin {
     $queue = @('$Hashtable') # initalize queue with hashtable object, in single quotes (literal)
-    $final = @() # final array with property and value hashtables
+    $output = @{} # output hashtable with property and value hashtables
     $complete = $false # while loop trigger
     
     <#
     .DESCRIPTION
-        Helper function to save path and value to $final array
+        Helper function to save path and value to $output hashtable
     .EXAMPLE
-        Add-ToFinal -Path $base.myProperty.thing -Value "12345"
+        Add-ToOutput -Path '$Hashtable.myProperty.thing' -Value '12345'
     #>
-    function Add-ToFinal {
+    function Add-ToOutput {
         [CmdletBinding()]
         [OutputType([System.Void])]
         param(
@@ -43,9 +56,8 @@ begin {
             [string] $Value
         )
         
-        $script:final += [ordered]@{
-            path  = "$Path"
-            value = "$Value"
+        $script:output += @{
+            "$Path" = "$Value"
         }
     }
 }
@@ -56,19 +68,32 @@ process {
         
         # loop through each item in $queue
         foreach ($qItem in $queue) {
-            $qItemType = (Invoke-Expression $qItem).GetType().Name
+            $qItemObject = (Invoke-Expression $qItem)
+            
+            if ($null -eq $qItemObject) {
+                # handle empty array, e.g. `myItemArray: []`
+                $qItemType = 'String' # empty arrays are considered a [String] datatype
+                $value = '[]'
+            } else {
+                # anything but an empty array
+                $qItemType = $qItemObject.GetType().Name
+                $value = (Invoke-Expression $qItem) # value of the current $qItem; used if $qItemType -eq 'String'
+            }
+            
             Write-Verbose "👀 Look at queue item: $qItem [$qItemType]"
             
-            if ($qItemType -eq 'String') {
-                # If queue item is a string, save to final and go to next $qItem
-                # > The reason we have to sections that call Add-ToFinal is because this one is used for
-                # > array values that are strings and the one below is for hash table values that are strings
+            if ($qItemType -match 'String' -or `
+                    $qItemType -match 'Boolean' -or `
+                    $qItemType -match 'Int' -or `
+                    $qItemType -match 'Double') {
+                # If queue item is a Scalar data type (String, Integer, Boolean, Decimal(Float)), save to output array and go to next $qItem
+                # > The reason we have to sections that call Add-ToOutput is because this one is used for
+                # > array values that are strings and the one below is for hashtable values that are strings
                 
-                $value = (Invoke-Expression $qItem) # value of the current $qItem
-                
-                Write-Verbose "➕ Add ${qItem}:${value} to final array"
-                Add-ToFinal -Path $qItem -Value $value
+                Write-Verbose "➕ Add ${qItem}:${value} to output array"
+                Add-ToOutput -Path $qItem -Value $value
             } else {
+                # $qItemType is a List or Dictionary
                 # go through child items of $qItem
                 
                 [int] $i = 0 # item counter, needed to label array items
@@ -85,12 +110,12 @@ process {
                     }
                     
                     if ($itemType -eq 'String') {
-                        # If queue item is a string, save to final and go to next $qItem
-                        # > The reason we have to sections that call Add-ToFinal is because this one is used for
-                        # > hash table values that are strings and the one above is for array values that are strings
+                        # If queue item is a string, save to output and go to next $qItem
+                        # > The reason we have to sections that call Add-ToOutput is because this one is used for
+                        # > hashtable values that are strings and the one above is for array values that are strings
                         
-                        Write-Verbose "➕ Add ${path}:${item} to final array"
-                        Add-ToFinal -Path "$path" -Value $item # value is the name of the current $qItem item
+                        Write-Verbose "➕ Add ${path}:${item} to output array"
+                        Add-ToOutput -Path "$path" -Value $item # value is the name of the current $qItem item
                     } else {
                         # add to $tempQueue
                         Write-Verbose "➕ Add '$path' to queue array"
@@ -113,6 +138,6 @@ process {
 }
 
 end {
-    # return $final as two column table
-    return $final | ForEach-Object { [PSCustomObject]$_ } | Format-Table -AutoSize -Wrap
+    # return single-level Object[] with property name path and value
+    return $output.GetEnumerator() | Sort-Object -Property Name
 }
